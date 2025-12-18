ac6e7565259aa55c7a552af03e71d10e80bfec25
rdulal619 <149902961+rdulal619@users.noreply.github.com>
2025-12-18 18:07:08 +0800

    Add SQL Q1: total bytes per protocol

--- sql/sql_diagnostic.sql
+++ sql/sql_diagnostic.sql
@@ -1,1 +1,9 @@
--- SQL Diagnostic File
+-- SQL Diagnostic: Network Traffic Analysis
+-- Author: Reshan Dulal
+
+-- Q1: Total bytes transferred per protocol
+SELECT
+    protocol,
+    SUM(bytes_transferred) AS total_bytes
+FROM network_logs
+GROUP BY protocol;
